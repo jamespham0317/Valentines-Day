da87bad6a0b46aaeef5aecbc60ca5176bf224263
jamespham <jpham07@student.ubc.ca>
2025-02-11 00:35:21 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import MessageBox from './components/MessageBox/MessageBox';
+import turtle from './components/Image/turtle.gif';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {message: 'start', 
+                  score: 1};
+  }
+
+  onButton = (correctness) => {
+    switch (this.state.message) {
+      case 'start':
+        this.setState({message: 'first'});
+        break;
+      case 'first':
+        if (correctness) {
+          this.setState({message: 'first-right', 
+                          score: this.state.score + 1});
+          break;
+        } else {
+          this.setState({message: 'first-wrong'});
+          break;
+        }
+      case 'first-right':
+      case 'first-wrong':
+        this.setState({message: 'second'});
+        break;
+      case 'second':
+        if (correctness) {
+          this.setState({message: 'second-right', 
+                          score: this.state.score + 1});
+          break;
+        } else {
+          this.setState({message: 'second-wrong'});
+          break;
+        }
+      case 'second-right':
+      case 'second-wrong':
+        this.setState({message: 'third'});
+        break;
+      case 'third':
+        this.setState({message: 'score'});
+        break;
+    }
+  }
+
+  moveButton = () => {
+    let noButton = document.getElementById("noButton");
+    let maxX = window.innerWidth - noButton.clientWidth - 1000; 
+    let maxY = window.innerHeight - noButton.clientHeight - 400;
+            
+    let newX = Math.random() * maxX;
+    let newY = Math.random() * maxY;
+
+    noButton.style.left = newX + "px";
+    noButton.style.top = newY + "px";
+  }
+
+  render() {
+    const { message, score } = this.state;
+    return (
+    <div className="App layout">
+      <img className='turtle' src={turtle} alt='turtle'/>
+      <MessageBox message={message} score={score} onButton={this.onButton} moveButton={this.moveButton}/>
+    </div>
+  );
+  }
+}
+export default App;
---
+++ src/components/MessageBox/MessageBox.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import celebrating from '../Image/celebrating.gif';
+import './MessageBox.css';
+
+const MessageBox = ({ message, score, onButton, moveButton }) => {
+  if (message === 'start') {
+    return (
+      <div className='message-box'>
+        <p> Quiz for May Wen </p>
+        <button className='startButton' onClick={onButton}>Start</button>
+      </div>
+    );
+  } else if (message === 'first') {
+    return (
+      <div className='message-box'>
+        <p> Question 1: What is my favourite colour? </p>
+        <div className='button-container'>
+          <button onClick={() => onButton(false)}>Blue</button>
+          <button onClick={() => onButton(true)}>Green</button>
+        </div>
+        <div className='button-container'>
+          <button onClick={() => onButton(false)}>Purple</button>
+          <button onClick={() => onButton(false)}>Black</button>
+        </div>
+      </div>
+    );
+  } else if (message === 'first-right') {
+    return (
+      <div className='message-box'>
+        <p> Correct! My favourite colour is in fact green! </p>
+        <button onClick={onButton}>Next Question</button>
+      </div>
+    );
+  } else if (message === 'first-wrong') {
+    return (
+      <div className='message-box'>
+        <p> Incorrect! My favourite colour is <br /> green. </p>
+        <button onClick={onButton}>Next Question</button>
+      </div>
+    );
+  } else if (message === 'second') {
+    return (
+      <div className='message-box'>
+        <p> Question 2: When is our anniversary? </p>
+        <div className='button-container'>
+          <button onClick={() => onButton(false)}>October 16th</button>
+          <button onClick={() => onButton(false)}>December 7th</button>
+        </div>
+        <div className='button-container'>
+          <button onClick={() => onButton(false)}>August 3rd</button>
+          <button onClick={() => onButton(true)}>January 5th</button>
+        </div>
+      </div>
+    );
+  } else if (message === 'second-right') {
+    return (
+      <div className='message-box'>
+        <p> Correct! Our anniversary is on January 5th! </p>
+        <button onClick={onButton}>Next Question</button>
+      </div>
+    );
+  } else if (message === 'second-wrong') {
+    return (
+      <div className='message-box'>
+        <p> Incorrect! Our anniversary is on January 5th. </p>
+        <button onClick={onButton}>Next Question</button>
+      </div>
+    );
+  } else if (message === 'third') {
+    return (
+      <div className='message-box'>
+        <p> Will you be my Valentine? </p>
+          <div style={{marginTop: '70px'}} className='button-container'>
+            <button id='yesButton' onClick={onButton}>Yes</button>
+            <button id='noButton' onMouseOver={moveButton}>No</button>
+          </div>
+      </div>
+    );
+  } else if (message === 'score') {
+    return (
+      <div className='message-box'>
+        <p style={{marginTop: '100px'}}> Yay! You got {Math.floor(score / 3 * 100)}% </p>
+        <img style={{marginTop: '-40px'}}className='celebrating' src={celebrating} alt='celebrating'/>
+      </div>
+    );
+  }
+  
+}
+
+export default MessageBox;
